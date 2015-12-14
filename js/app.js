933faaee0dc897465c8c305933b4ea01cd26162b
Matthew Marcus <mmarcus@fanatics.com>
2015-12-14 12:50:48 -0800

    multiple containers making multiple calls working

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 import 'babel/polyfill';
 
-import App from './components/App';
+//import App from './components/App';
+import Widgets1 from './components/Widgets1';
+import Widgets2 from './components/Widgets2';
 import AppHomeRoute from './routes/AppHomeRoute';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Relay from 'react-relay';
 
+let route = new AppHomeRoute();
+
 ReactDOM.render(
-  <Relay.RootContainer
-    Component={App}
-    route={new AppHomeRoute()}
-  />,
+  <div>
+    <Relay.RootContainer
+      Component={Widgets1}
+      route={route}
+    />
+    <Relay.RootContainer
+      Component={Widgets2}
+      route={route}
+    />
+  </div>,
   document.getElementById('root')
 );
--- js/components/Widgets2.js
+++ js/components/Widgets2.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import Relay from 'react-relay';
-import Widgets1 from './Widgets1';
-import Widgets2 from './Widgets2';
 
-class App extends React.Component {
+class Widgets2 extends React.Component {
   render() {
+    console.log(this.props)
     return (
       <div>
-        <Widgets1 />
-        <Widgets2 />
+        <h1>Widget list 2</h1>
+        <ul>
+          {this.props.viewer.widgets.edges.map(edge =>
+            <li key={edge.node.id}>{edge.node.name} (ID: {edge.node.id})</li>
+          )}
+        </ul>
       </div>
     );
   }
 }
 
-export default Relay.createContainer(App,
-{
+export default Relay.createContainer(Widgets2, {
   fragments: {
     viewer: () => Relay.QL`
       fragment on User {
